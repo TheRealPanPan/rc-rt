9f5b33fe5020986a2339750e08c37fd8ae6712e1
TheRealPanPan <therealpanpan@gmail.com>
2017-01-21 12:26:50 +0100

    Add Validation to tool + first plate done

--- plates/plate_files_generator.py
+++ plates/plate_files_generator.py
@@ -28,10 +28,10 @@ objective_type = [
 ]
 
 
-def ask_urld_for_tile(urld):
+def ask_urdl_for_tile(urdl):
     v = ""
     while v not in ["0", "1"]:
-        v = input("%s [1]: " % urld)
+        v = input("%s [1]: " % urdl)
 
         if not v:
             v = "1"
@@ -39,10 +39,14 @@ def ask_urld_for_tile(urld):
     return v
 
 
+def calculate_id(x, y):
+    return "%s" % ((x * 100) + y)
+
+
 def ask_info_for_tile(x, y):
     t_type = ""
     o_type = ""
-    t_id = "%s" % ((x * 100) + y)
+    t_id = calculate_id(x, y)
     while t_type not in tile_type:
         t_type = input(
             "Type of Tile at (Col,Line) (%s,%s)"
@@ -55,9 +59,9 @@ def ask_info_for_tile(x, y):
             while o_type not in objective_type:
                 o_type = input("What Objective is it :")
 
-        urlds = {}
-        for urld in ["UP", "RIGHT", "DOWN", "LEFT"]:
-            urlds[urld] = ask_urld_for_tile(urld)
+        urdls = {}
+        for urdl in ["UP", "RIGHT", "DOWN", "LEFT"]:
+            urdls[urdl] = ask_urdl_for_tile(urdl)
 
     tile = {
         "id": t_id,
@@ -66,11 +70,13 @@ def ask_info_for_tile(x, y):
             "y": y
         },
         "type": tile_type[t_type],
-        "urdl": "%s%s%s%s" % (
-            urlds["UP"],
-            urlds["RIGHT"],
-            urlds["DOWN"],
-            urlds["LEFT"]
+        "urdl": int(
+            "%s%s%s%s" % (
+                urdls["UP"],
+                urdls["RIGHT"],
+                urdls["DOWN"],
+                urdls["LEFT"]
+            )
         )
     }
 
@@ -80,11 +86,87 @@ def ask_info_for_tile(x, y):
     return tile
 
 
+def is_up_authorized(tile):
+    return True if tile["urdl"] >= 1000 else False
+
+
+def is_left_authorized(tile):
+    return True if tile["urdl"] % 2 != 0 else False
+
+
+def is_right_authorized(tile):
+    urdl = tile["urdl"]
+    if urdl >= 1000:
+        urdl = urdl - 1000
+
+    return True if urdl >= 100 else False
+
+
+def is_down_authorized(tile):
+    urdl = tile["urdl"]
+    if urdl >= 1000:
+        urdl = urdl - 1000
+
+    if urdl >= 100:
+        urdl = urdl - 100
+
+    return True if urdl >= 10 else False
+
+
+def add_to_collection(tiles, tile):
+    if tile["coord"]["y"] == 1:  # First Line cannot go up.
+        if is_up_authorized(tile):  # Means UP If authorized
+            print("You can't go up on firt line ...")
+            return True
+
+    if tile["coord"]["x"] == 1:  # First Row cannot go left.
+        if is_left_authorized(tile):  # Not pair, mean Left is authorized
+            print("you can't go left on first row ...")
+            return True
+
+    if tile["coord"]["x"] == 8:  # Last col should go right
+        if not is_right_authorized(tile):
+            print("you should go right on last col ...")
+            return True
+
+    if tile["coord"]["y"] == 8:  # Last row should go down except on WHIRLPOOL
+        if not is_down_authorized(tile) and tile["type"] != tile_type["W"]:
+            print("you should go down on last row ...")
+            return True
+
+    if tile["coord"]["x"] == 1 and tile['coord']['y'] == 1:
+        tiles[tile["coord"]["y"]][tile["coord"]["x"]] = tile
+        return False  #it's the first tile
+
+    # Looking at upper tile for cohesion
+    if tile["coord"]["y"] != 1:
+        if is_down_authorized(
+            tiles[tile["coord"]["y"]-1][tile["coord"]["x"]]
+        ) != is_up_authorized(tile):
+            print("Superior tile is not compatible with this one")
+            return True
+
+    # looking left at you
+    if tile["coord"]["x"] != 1:
+        if is_right_authorized(
+            tiles[tile["coord"]["y"]][tile["coord"]["x"]-1]
+        ) != is_left_authorized(tile):
+            print("Right tile is not compatible with this one")
+            return True
+
+    tiles[tile["coord"]["y"]][tile["coord"]["x"]] = tile
+    return False  # All is ok
+
+
 if __name__ == "__main__":
-    tiles = []
+    tiles = {}
     for y in range(1, 9):
+        tiles[y] = {}
         for x in range(1, 9):
-            tiles.append(ask_info_for_tile(x, y))
+            b = True
+            while b:
+                tile = ask_info_for_tile(x, y)
+                b = add_to_collection(tiles, tile)
 
     with open("plates_01.json", "wb") as f:
         f.write(json.dumps(tiles).encode("UTF-8"))
